80a8f386297f1b92e0cb78d3975501b68822a075
AndreastiUia <andreasti@uia.no>
2024-02-07 21:21:35 +0100

    Res_option_button now disabled if fullscreen is toggeled on

--- Scripts/main_menu_options.gd
+++ Scripts/main_menu_options.gd
@@ -1,21 +1,34 @@
 extends Control
 
-@onready var resolution_option_button = $Background/OptionsContainer/OptionButton
+@onready var res_option_button = $Background/OptionsContainer/OptionButton
 
 
 # Dictionary for the selectable resolutions
 var Resolutions: Dictionary = {"3440x1440":Vector2i(3440,1440),
 								"2560x1440":Vector2i(2560,1440),
 								"1920x1080":Vector2i(1920,1080),
-								"1280x720":Vector2i(1280,720),
 								"1440x900":Vector2i(1440,900),
+								"1280x720":Vector2i(1280,720),
 								"1024x600":Vector2i(1024,600),
 								"800x600":Vector2i(800,600)}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Background/LeftSideButtons/BackButton.grab_focus()
+	Resolution_label_set()
 	Add_Resolutions()
+	Check_Fullscreen()
+
+func Check_Fullscreen():
+	var _window = get_window()
+	var mode = _window.get_mode()
+	
+	if mode == Window.MODE_FULLSCREEN:
+		res_option_button.set_disabled(true)
+
+func Resolution_label_set():
+	var res_text = str(get_window().get_size().x)+"x"+str(get_window().get_size().y)
+	res_option_button.set_text(res_text)
 
 # Populate the options button with resolutions and check current resolution
 func Add_Resolutions():
@@ -23,9 +36,9 @@ func Add_Resolutions():
 	var ID = 0
 	
 	for r in Resolutions:
-		resolution_option_button.add_item(r, ID)
+		res_option_button.add_item(r, ID)
 		if Resolutions[r] == Current_Resolution:
-			resolution_option_button.select(ID)
+			res_option_button.select(ID)
 		
 		ID += 1
 
@@ -34,7 +47,7 @@ func _on_back_button_pressed():
 
 
 func _on_option_button_item_selected(index):
-	var ID = resolution_option_button.get_item_text(index)
+	var ID = res_option_button.get_item_text(index)
 	get_window().set_size(Resolutions[ID])
 	Center_Window()
 
@@ -44,7 +57,7 @@ func Center_Window():
 	get_window().set_position(Center_Screen-Window_Size/2)
 
 func _on_check_button_toggled(toggled_on):
-	# resolution_option_button.set_disabled(toggled_on)
+	res_option_button.set_disabled(toggled_on)
 	
 	if toggled_on:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
