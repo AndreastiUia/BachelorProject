60915dde20c55fa2c233c43223796f12b6875260
Kevin <kevinsyltoy@gmail.com>
2024-02-09 10:09:03 +0100

    Added draggable objects that snaps together to reperesent codeblocks

--- Scripts/GameStateManager.gd
+++ Scripts/GameStateManager.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
-# Singleton instance
+
 static var instance
 
-# State of the game
+
 var game_paused = false
 
 func _ready():
 	instance = self
 
-# Pause the game
+
 func pause_game():
 	game_paused = true
-	# Optionally, you can add code here to pause physics, animations, etc.
+	
 
-# Resume the game
+
 func resume_game():
 	game_paused = false
-	# Optionally, you can add code here to resume physics, animations, etc.
+	
 
-# Check if the game is paused
+
 func is_game_paused():
 	return game_paused
