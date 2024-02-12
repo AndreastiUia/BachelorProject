331ee93733e5ca03c5428ab58ce61105a786d326
Matias Berge <MatiasB@UIA.no>
2024-02-12 19:44:36 +0100

    Added damage and reapoir to health component

--- healthComponent.gd
+++ healthComponent.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
-var MAX_HEALTH: int = 100
+@export var MAX_HEALTH: int = 100
 var health: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = MAX_HEALTH
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func take_damage(damage: int):
+	# Take damage.
+	health -= damage
+
+func repair(repair: int):
+	# Repair bot.
+	health += repair
