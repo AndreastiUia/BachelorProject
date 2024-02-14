d7f02eb0d091e3bb03375fc0f1b5c9616e4fe377
AndreastiUia <andreasti@uia.no>
2024-02-14 12:59:37 +0100

    Fixed some placements

--- Scripts/upgrade_screen.gd
+++ Scripts/upgrade_screen.gd
@@ -1,8 +1,5 @@
 extends Control
 
-func _ready():
-	hide()
-
 func upgradescreen():
 	show()
 	get_tree().paused = true
@@ -20,26 +17,26 @@ func _on_back_button_pressed():
 	quitupgradescreen()
 
 func _on_base_tab_button_pressed():
-	if $CanvasLayer/Panel/BasePanel.visible == false:
-		$CanvasLayer/Panel/BasePanel.visible = true
-		$CanvasLayer/Panel/BotPanel.visible = false
-		$CanvasLayer/Panel/BotPanel2.visible = false
+	if $GUI/BasePanel.visible == false:
+		$GUI/BasePanel.visible = true
+		$GUI/BotPanel.visible = false
+		$GUI/BotPanel2.visible = false
 	else:
 		pass
 	
 
 func _on_bot_tab_button_pressed():
-	if $CanvasLayer/Panel/BotPanel.visible == false:
-		$CanvasLayer/Panel/BasePanel.visible = false
-		$CanvasLayer/Panel/BotPanel.visible = true
-		$CanvasLayer/Panel/BotPanel2.visible = false
+	if $GUI/BotPanel.visible == false:
+		$GUI/BasePanel.visible = false
+		$GUI/BotPanel.visible = true
+		$GUI/BotPanel2.visible = false
 
 
 func _on_bot_tab_button_2_pressed():
-	if $CanvasLayer/Panel/BotPanel2.visible == false:
-		$CanvasLayer/Panel/BasePanel.visible = false
-		$CanvasLayer/Panel/BotPanel.visible = false
-		$CanvasLayer/Panel/BotPanel2.visible = true
+	if $GUI/BotPanel2.visible == false:
+		$GUI/BasePanel.visible = false
+		$GUI/BotPanel.visible = false
+		$GUI/BotPanel2.visible = true
 
 
 
