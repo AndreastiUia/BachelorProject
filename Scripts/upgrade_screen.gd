06f4193a17fa9e5acf4f3665805a2555d9118c15
AndreastiUia <andreasti@uia.no>
2024-02-22 14:11:13 +0100

    Upgrade screen now works like programbot screen

--- Scripts/upgrade_screen.gd
+++ Scripts/upgrade_screen.gd
@@ -1,45 +1,23 @@
 extends Control
 
+#Temp back button for easier testing
+func _process(delta):
+	# Go back to main menu
+	if Input.is_action_pressed("quit_to_menu"):
+		resume()
+
 func upgradescreen():
 	show()
 	get_tree().paused = true
-	#AnimationPlayer.play("blur")
+	$AnimationPlayer.play("blur")
 
-func quitupgradescreen():
+func resume():
 	hide()
 	get_tree().paused = false
-	#$AnimationPlayer.play_backwards("blur")
+	$AnimationPlayer.play_backwards("blur")
 
 func _on_upgrade_screen_pressed():
 	upgradescreen()
 
-func _on_back_button_pressed():
-	quitupgradescreen()
-
-func _on_base_tab_button_pressed():
-	if $BasePanel.visible == false:
-		$BasePanel.visible = true
-		$BotPanel.visible = false
-		$BotPanel2.visible = false
-	else:
-		pass
-	
-
-func _on_bot_tab_button_pressed():
-	if $BotPanel.visible == false:
-		$BasePanel.visible = false
-		$BotPanel.visible = true
-		$BotPanel2.visible = false
-
-
-func _on_bot_tab_button_2_pressed():
-	if $BotPanel2.visible == false:
-		$BasePanel.visible = false
-		$BotPanel.visible = false
-		$BotPanel2.visible = true
-
-
-
-
-
-
+func _on_return_button_pressed():
+	resume()
