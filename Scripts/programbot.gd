00054e15f4ab66457ca115b97893aa4cf03bb7fc
Kevin Syltøy <kevinls@uia.no>
2024-02-29 11:33:40 +0100

    Does not add duplicates of the same robot in the robot list in programrobot scene

--- Scripts/programbot.gd
+++ Scripts/programbot.gd
@@ -16,15 +16,19 @@ func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 
+# To keep track of allready added robots
+var added_bots = {}
+
 func _on_program_robots_pressed():
 	programbots()
 	
-	#Add Robots to Robot list on programbot scene
 	for bot in Global.bots:
-		var label = Label.new()
-		label.text = str(bot)
-		print("Adding bot:", str(bot))
-		$BackdropPanel/Robots.add_child(label)
+		if bot not in added_bots:
+			var label = Label.new()
+			label.text = str(bot)
+			print("Adding bot:", str(bot))
+			$BackdropPanel/Robots.add_child(label)
+			added_bots[bot] = true
 
 
 func _on_temp_return_pressed():
