73cdd465dbf66163ad2958ea6a601b2f5190a130
Kevin Syltøy <kevinls@uia.no>
2024-03-05 12:22:00 +0100

    Moved ui parts to better fit screen

--- Scenes/CoordinateInputDialog.gd
+++ Scenes/CoordinateInputDialog.gd
@@ -1,17 +1,6 @@
 extends Control
 
-signal coordinates_entered(Vector2)
+func _on_confirm_pressed():
+	
 
-func _on_confirm_button_pressed():
-	var x_str = $XCoordinateLineEdit.text.strip_edges()
-	var y_str = $YCoordinateLineEdit.text.strip_edges()
-
-	# Convert input to integers
-	var x = int(x_str) if x_str.is_valid_integer() else 0
-	var y = int(y_str) if y_str.is_valid_integer() else 0
-
-	# Emit signal with entered coordinates
-	emit_signal("coordinates_entered", Vector2(x, y))
-
-	# Close the dialog
 	hide()
