7b7ca01e15d3b11807020cb53a2fdf7cd758b2ac
AndreastiUia <andreasti@uia.no>
2024-03-09 11:00:52 +0100

    Robotlist is now its own scene

--- Scripts/upgrade_screen.gd
+++ Scripts/upgrade_screen.gd
@@ -1,15 +1,5 @@
 extends Control
 
-
-func _ready():
-	pass
-
-#Temp back button for easier testing
-func _process(delta):
-	# Go back to main menu
-	if Input.is_action_pressed("quit_to_menu"):
-		resume()
-
 func upgradescreen():
 	show()
 	get_tree().paused = true
@@ -20,20 +10,8 @@ func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
 
-#Add bots to Robotlist in upgradescreen
-func populate_bot_list():
-	for bot in Global.bots:
-		$TabContainer/Robots/RobotList.add_item(str(bot), null)
-		print("Adding bot:", str(bot))
-
-#Clears the RobotList when resuming from upgradescreen
-func clear_bot_list():
-	$TabContainer/Robots/RobotList.clear()
-
 func _on_upgrade_screen_pressed():
 	upgradescreen()
-	populate_bot_list()
 
 func _on_return_button_pressed():
 	resume()
-	clear_bot_list()
