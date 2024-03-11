997068a5be716af81c917139f2bc48ef3341a0a5
AndreastiUia <andreasti@uia.no>
2024-03-11 21:21:17 +0100

    Base upgrades now increase global vars

--- Scripts/global_vars.gd
+++ Scripts/global_vars.gd
@@ -1,7 +1,11 @@
 extends Node
 
 # Base variables
+<<<<<<< HEAD
 var base_gold = 500
+=======
+var base_gold = 0
+>>>>>>> d4226e4 (Base upgrades now increase global vars)
 var base_wood = 0
 var base_stone = 0
 
