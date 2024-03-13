c93683e1e558b0285c8096f6fdc4d9035cc40e69
AndreastiUia <andreasti@uia.no>
2024-03-13 19:25:55 +0100

    Added the rest of the upgrades

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -7,7 +7,12 @@ var bot = preload("res://Scenes/bot.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var b = bot.instantiate()
+	var position = Vector2(25, -20)
+	b.position = position
+	Global.bots.append(b)
+	print(Global.bots)
+	add_child(b)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
