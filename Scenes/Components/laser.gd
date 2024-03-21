830ad7650487e800ebe8fd10a17d53be8ba1cbfc
Matias Berge <matiasb@uia.no>
2024-03-21 15:59:59 +0100

    Laser give damage to bots that is hit. And the bot is deleted if health reaches zero.

--- Scenes/Components/laser.gd
+++ Scenes/Components/laser.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@export var speed = -50
+@export var speed = -200
+@export var damage = 10
 
 var pos:Vector2
 var direction
@@ -8,8 +9,8 @@ var target = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	direction = (global_position-target).normalized()
-	global_rotation = global_position.direction_to(target).angle()
+	direction = (global_position-target.global_position).normalized()
+	global_rotation = global_position.direction_to(target.global_position).angle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -24,4 +25,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_body_entered(body):
 	if body.has_method("program_bot"):
+		if body.has_method("damage"):
+			body.damage(damage)
 		queue_free()
