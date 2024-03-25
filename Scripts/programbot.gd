9e0bf001c82d44d9f4bdaf4e8b605de247f2a979
Matias Berge <matiasb@uia.no>
2024-03-25 13:06:34 +0100

    Programlist is cleared when exiting program window. First bot in the list is selected when opening prog window.

--- Scripts/programbot.gd
+++ Scripts/programbot.gd
@@ -14,13 +14,14 @@ func resume():
 	hide()
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	get_node("BackdropPanel/ActiveProgram").clear()
+	get_node("BackdropPanel/Programs").clear()
 
 # To keep track of allready added robots
 var added_bots = {}
 
 func _on_program_robots_pressed():
 	programbots()
-	
 	var bots = get_node("BackdropPanel/RobotlistControlNode/RobotList")
 	if bots.is_anything_selected():
 		get_node("BackdropPanel/ActiveProgram").get_current_program()
--- Scripts/robotlist.gd
+++ Scripts/robotlist.gd
@@ -1,18 +1,20 @@
 extends Control
 signal _on_select(index:int)
 
+@onready var robot_list = $RobotList
+
 var bot
 
 
 #Add bots to Robotlist in upgradescreen
 func populate_bot_list():
 	for bot in Global.bots:
-		$RobotList.add_item(str(bot.botname), null, true)
+		robot_list.add_item(str(bot.botname), null, true)
 
 
 #Clears the RobotList when resuming from upgradescreen
 func clear_bot_list():
-	$RobotList.clear()
+	robot_list.clear()
 
 #Clear robotlist on robotlist hidden
 func _on_robot_list_hidden():
@@ -21,6 +23,11 @@ func _on_robot_list_hidden():
 #Populate robotlist on RobotlistControlNode draw
 func _on_draw():
 	populate_bot_list()
+	if Global.bots.is_empty():
+		return
+	robot_list.select(0)
+	bot = Global.bots[0]
+	emit_signal("_on_select", 0)
 
 #Store the selected robotlist item
 func _on_robot_list_item_selected(index):
