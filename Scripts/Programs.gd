55d3a74dfbb939814a49fbc2db6df0f6155ab608
Matias Berge <matiasb@uia.no>
2024-04-15 11:51:21 +0200

    Made a global Tooltips.gd to store tooltips in.

--- Scripts/Programs.gd
+++ Scripts/Programs.gd
@@ -64,13 +64,12 @@ func _on_focus_exited():
 func _on_robotlist_control_node__on_select(index):
 	var bot = Global.bots[index]
 	clear()
-	var file = FileAccess.open(PROG_TOOLTIP, FileAccess.READ)
 	for i in bot.program_func:
 		var text = str(i)
 		# Do not add *_END to the list.
 		if text.contains("END"):
 			continue
-		#var tooltip = file.get_line()
+		var tooltip = Tooltips.prog_tooltips[item_count]
 		add_item(i)
-		#set_item_tooltip(item_count-1, tooltip)
+		set_item_tooltip(item_count-1, tooltip)
 		
---
+++ Tooltip/tooltips.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var prog_tooltips = ["Move one tile left.",
+	"Move one tile right.",
+	"Move one tile up.",
+	"Move one tile down.",
+	"Move to a specific position on the map, if the posistion is uncovered and not occupied by a resource or the base.",
+	"Start of a while-loop, this will repeat the functions between the WHILE and WHILE_END until the conditions is no longer met, or the loop is broken with the WHILE_BREAK.",
+	"Break out of the current while-loop.",
+	"Use this to create an IF-statement, this will check if a selected statement is TRUE and only then run the program between the IF and next IF_END.",
+	"Collect one resource of an adjacent resourse, if there is any. The bot will only collect one item for each time this command is ran.",
+	"Deliver one resource if the bot is standing next to the base. The bot will only collect one item for each time this command is ran."
+	]
